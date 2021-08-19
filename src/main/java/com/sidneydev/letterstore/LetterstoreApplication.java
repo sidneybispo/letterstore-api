e2fab048409d4814fd26e21761e8b9aef51a855c
sidneybispo <protecney@gmail.com>
2021-08-19 16:54:40 -0300

    commit inicial

---
+++ src/main/java/com/sidneydev/letterstore/LetterstoreApplication.java
@@ -0,0 +1,13 @@
+package com.sidneydev.letterstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetterstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetterstoreApplication.class, args);
+	}
+
+}

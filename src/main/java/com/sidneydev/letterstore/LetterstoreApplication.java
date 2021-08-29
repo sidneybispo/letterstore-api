4171ddb3cb59eb31b479a024ab1d0829eb85c6b9
sidneybispo <protecney@gmail.com>
2021-08-29 12:53:54 -0300

    instancia de forma automatica da base de dados

--- src/main/java/com/sidneydev/letterstore/LetterstoreApplication.java
+++ src/main/java/com/sidneydev/letterstore/LetterstoreApplication.java
@@ -1,13 +1,40 @@
-package com.sidneydev.letterstore;
+package com.sidneydev.letterstore; // Autor: Sidney Bispo
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class LetterstoreApplication {
+import com.sidneydev.letterstore.domain.Categoria;
+import com.sidneydev.letterstore.domain.Livro;
+import com.sidneydev.letterstore.repositories.CategoriaRepository;
+import com.sidneydev.letterstore.repositories.LivroRepository;
 
+@SpringBootApplication
+public class LetterstoreApplication implements CommandLineRunner{
+	
+	@Autowired
+    private CategoriaRepository categoriaRepository;
+	@Autowired
+	private LivroRepository livroRepository;
+    
 	public static void main(String[] args) {
 		SpringApplication.run(LetterstoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica", "Livros de TI");
+		
+		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Lorem ipsum", cat1);
+		
+		cat1.getLivros().addAll(Arrays.asList(l1));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
+		
 	}
 
 }
